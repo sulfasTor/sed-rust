e66ad9c7de95f172a8cb39c3831ea7824db51527
sulfastor <torresmoisesa@gmail.com>
2025-02-03 13:44:54 -0600

    Handle stdin

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env::args;
-use std::fs::read_to_string;
+use std::fs;
+use std::io;
 use std::process;
 
 const VERSION: &str = "0.0.1";
@@ -8,23 +9,42 @@ fn help() {
     let usage = "
 Usage: sed-rust [help h] [version v]... {script-only-if-no-other-script} [input-file]...
 ";
-    println!("{usage}");
+    print!("{usage}");
 }
 
 fn version() {
-    println!("{}", VERSION);
+    print!("{}", VERSION);
 }
 
 fn parse_script(s: &str) -> &str {
     s
 }
 
-fn handle_script(path: &str, script: &str) {
-    let file = read_to_string(path).unwrap_or_else(|err| {
-      eprintln!("{}: {}", path, err);
-      process::exit(1);
-    });
-    println!("{}", file)
+struct FilesBuffer {
+    files: Vec<String>,
+}
+
+fn handle_script(script: &str, files: Option<&[String]>) {
+    let fb = match files {
+        Some([]) | None => {
+            let buf = io::read_to_string(io::stdin()).unwrap();
+            FilesBuffer { files: vec![buf] }
+        }
+        Some(files) => {
+            let mut buffers = vec![];
+            for f in files {
+                let file = fs::read_to_string(f.to_string()).unwrap_or_else(|err| {
+                    eprintln!("{}: {}", f.to_string(), err);
+                    process::exit(1);
+                });
+                buffers.push(file);
+            }
+            FilesBuffer { files: buffers }
+        }
+    };
+    let cmd = parse_script(script);
+    println!("{}", cmd);
+    println!("{:?}", fb.files)
 }
 
 fn handle_args() {
@@ -35,16 +55,21 @@ fn handle_args() {
         match arg.as_str() {
             "--help" | "-h" => {
                 help();
-                break;
+                process::exit(0);
             }
             "--version" | "-v" => {
                 version();
-                break;
+                process::exit(0);
             }
             _ => pos.push(arg),
         }
     }
-    handle_script(pos.last().unwrap(), pos.first().unwrap());
+    if pos.len() == 0 {
+        eprint!("Missing script positional argument");
+        help();
+        process::exit(1);
+    }
+    handle_script(pos.last().unwrap(), pos.get(1..));
 }
 
 fn main() {
